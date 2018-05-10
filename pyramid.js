6b78e16afe8bb745f7c7c6a5f1344dcd5b61d297
User <user@Mac.local>
2018-05-11 00:45:21 +1000

    added pyramid solution

---
+++ pyramid.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+function pyramid(n) { 
+  let midPoint = Math.floor((2*n-1)/2);
+  for (let row = 0; row < n; row++){
+    let level = "" 
+    for (let col = 0; col < 2 * n - 1; col++){
+      if( midPoint - row <= col && midPoint + row >= col){
+          level = level + "#"
+      }else{
+        level = level + " ";
+     }
+    }console.log(level);
+  }
+}
